e493186e424bc053f2ace3a905e0f90f33b9d0d2
Metin <metinkonuk@hotmail.com>
2021-09-20 17:37:39 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          METIN KONUK
+          METIN KONUK SİTESİ DEĞİŞTİ
         </a>
       </header>
     </div>
